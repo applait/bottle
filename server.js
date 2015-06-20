a7390ec5520eb34f2870c681ef93b8e187a19a69
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-06-20 21:15:12 +0530

    Namespace IRC connections. Separate out Freenode's client
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server.js
+++ server.js
@@ -2,21 +2,21 @@
  * Bottle server start
  */
 
-var irc = require("irc");
+var irc = { module: require("irc"), servers: {}, clients: {} };
 
 var config = require("./config");
 
 // Spin off IRC client instance
-var irc_client = new irc.Client(config.irc.server, config.irc.nick, {
-    userName: config.irc.userName || "_applaitbot_",
-    realName: config.irc.realName || "Applait Bot",
-    password: config.irc.password,
-    port: config.irc.port || 6697,
-    channels: config.irc.channels || [],
-    secure: config.irc.secure || false,
-    selfSigned: config.irc.selfSigned || true,
-    certExpired: config.irc.certExpired || false,
-    sasl: config.irc.sasl || false,
+irc.clients.freenode = new irc.module.Client(config.irc.freenode.server, config.irc.freenode.nick, {
+    userName: config.irc.freenode.userName || "_applaitbot_",
+    realName: config.irc.freenode.realName || "Applait Bot",
+    password: config.irc.freenode.password,
+    port: config.irc.freenode.port || 6697,
+    channels: config.irc.freenode.channels || [],
+    secure: config.irc.freenode.secure || false,
+    selfSigned: config.irc.freenode.selfSigned || true,
+    certExpired: config.irc.freenode.certExpired || false,
+    sasl: config.irc.freenode.sasl || false,
     autoRejoin: true,
     autoConnect: true,
     showErrors: true,
