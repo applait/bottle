d3bd1baae304d01606dd7a4757a4044380269183
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-06-20 22:14:33 +0530

    First test of integrating Github webhook with push event
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
  * Bottle server start
  */
 
-var irc = { module: require("irc"), servers: {}, clients: {} };
+var irc = { module: require("irc"), servers: {}, clients: {} },
+    util = require("util"),
+    config = require("./config");
 
-var config = require("./config");
+// set up github webhook listener
+var gh_webhook = require('githubhook')({
+        host: config.github.webhook.ip || "0.0.0.0",
+        port: config.github.webhook.port || 8081,
+        path: config.github.webhook.path || "push",
+        secret: config.github.webhook.secret
+    });
 
 // Spin off IRC client instance
 irc.clients.freenode = new irc.module.Client(config.irc.freenode.server, config.irc.freenode.nick, {
@@ -23,3 +31,14 @@ irc.clients.freenode = new irc.module.Client(config.irc.freenode.server, config.
     retryDelay: 5000,
     debug: true
 });
+
+// listen to push on github on branch master
+gh_webhook.on('push', function (repo, ref, data) {
+    util.log("GH push", data);
+    irc.clients.freenode.say("#applait",
+                             ["[Github]", data.pusher.name, "pushed", data.commits.length, "commits to",
+                              data.repository.full_name, data.compare].join(" "));
+});
+
+// listen to github push
+gh_webhook.listen();
